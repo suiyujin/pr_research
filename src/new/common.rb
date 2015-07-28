c34fa2033f452a8e4c7534712e0667948f120bdd
suiyujin <suiyujin04@gmail.com>
2015-07-28 17:28:40 +0900

    use constants.yml and add constants for experiment of midterm

--- src/new/common.rb
+++ src/new/common.rb
@@ -4,23 +4,42 @@ require 'date'
 require 'logger'
 
 module Common
-  SECRETS = YAML.load_file("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}config/secrets.yml").freeze
+  secrets = YAML.load_file("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}config/secrets.yml")
+  constants = YAML.load_file("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}config/constants.yml")
   
-  PAGE = 'yahoo'.freeze
-  MY = Mysql.new(SECRETS['mysql_config']['host'], SECRETS['mysql_config']['user'], SECRETS['mysql_config']['password'], "crawler_#{PAGE}")
-  MY.charset = 'utf8'
-  SKIP_DATES = [].freeze
-  START_DATE = Date.new(2014, 1, 11).freeze
-  END_DATE = Date.new(2014, 1, 31).freeze
-  MATLAB = true.freeze
+  # page ID
+  PAGE = constants['page']
 
-  LOGFILE_NAME = "refactor_#{PAGE}_#{START_DATE.strftime('%Y%m%d')}_#{END_DATE.strftime('%Y%m%d')}.log".freeze
+  # mysql connect
+  MY = Mysql.new(secrets['mysql_config']['host'], secrets['mysql_config']['user'], secrets['mysql_config']['password'], "crawler_#{PAGE}")
+  MY.charset = 'utf8'
+  
+  # date
+  START_DATE = constants['start_date']
+  END_DATE = constants['end_date']
+  SKIP_DATES = constants['skip_dates']
+  A_DATE = constants['a_date']
+  B_DATE = constants['b_date']
+  N_DATE = constants['n_date']
+  M_DATE = constants['m_date']
+
+  # flag
+  MATLAB = constants['matlab']
+
+  # log
+  LOGFILE_NAME = "refactor_#{PAGE}_#{START_DATE.strftime('%Y%m%d')}_#{END_DATE.strftime('%Y%m%d')}.log"
   LOG = Logger.new("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}log/#{LOGFILE_NAME}")
   LOG.level = Logger::INFO
-
-  N_DATE = 7
   
-  RESULTFILE_DIR = File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '') + 'result/'.freeze
+  # path
+  RESULTFILE_DIR = File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '') + 'result/'
+
+  # threshold
+  TH_MORE_INCS = constants['th_more_incs']
+
+  # params
+  REDUCE_WEIGHT = constants['reduce_weight']
+  TAIL_OF_FILE = constants['tail_of_file']
 
   def matlab?
     MATLAB
