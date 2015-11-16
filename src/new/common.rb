3742120019920e9069247887cc2651342a118bb9
suiyujin <suiyujin04@gmail.com>
2015-11-16 12:26:04 +0900

    remove spaces

--- src/new/common.rb
+++ src/new/common.rb
@@ -6,14 +6,14 @@ require 'logger'
 module Common
   secrets = YAML.load_file("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}config/secrets.yml")
   constants = YAML.load_file("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}config/constants.yml")
-  
+
   # page ID
   PAGE = constants['page']
 
   # mysql connect
   MY = Mysql.new(secrets['mysql_config']['host'], secrets['mysql_config']['user'], secrets['mysql_config']['password'], "crawler_#{PAGE}")
   MY.charset = 'utf8'
-  
+
   # date
   START_DATE = constants['start_date']
   END_DATE = constants['end_date']
@@ -30,7 +30,7 @@ module Common
   LOGFILE_NAME = "refactor_#{PAGE}_#{START_DATE.strftime('%Y%m%d')}_#{END_DATE.strftime('%Y%m%d')}.log"
   LOG = Logger.new("#{File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '')}log/#{LOGFILE_NAME}")
   LOG.level = Logger::INFO
-  
+
   # path
   RESULTFILE_DIR = File.expand_path(File.dirname(__FILE__)).sub(/src\/new/, '') + 'result/'
 
@@ -54,7 +54,7 @@ module Common
     puts "++++++++++++++++++"
     LOG.info("++++++++++++++++++")
   end
-  
+
   def print_variable(variable)
     variable.each do |name, value|
       p "#{name}: #{value}"
