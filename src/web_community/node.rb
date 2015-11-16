c78128c696f90edd3beb3879566edde28e3ab682
suiyujin <suiyujin04@gmail.com>
2015-11-16 13:52:18 +0900

    create node from url

--- src/web_community/node.rb
+++ src/web_community/node.rb
@@ -1,3 +1,38 @@
+require 'cgi'
+require File.expand_path(File.dirname(__FILE__)).sub(/web_community/, 'new') + '/common'
+
 class Node
-  attr_accessor :id, :url_hash
+  include Common
+
+  attr_accessor :id, :url, :url_hash
+
+  def initialize(url)
+    @url = url.include?('%3A%2F%2F') ? url : encode_url(url)
+    @url_hash = get_hash_from_url(url)
+  end
+
+  def encode_url(url)
+    url.include?('%3A%2F%2F') ? url : CGI.escape(url)
+  end
+
+  def decode_url(url)
+    url.include?('%3A%2F%2F') ? CGI.unescape(url) : url
+  end
+
+  def get_url_from_hash(hash)
+    MY.query("
+      SELECT url
+      FROM urls
+      WHERE url_hash = '#{hash}';
+             ").fetch_row[0]
+  end
+
+  def get_hash_from_url(url)
+    en_url = url.include?('%3A%2F%2F') ? url : encode_url(url)
+    MY.query("
+      SELECT url_hash
+      FROM urls
+      WHERE url = '#{en_url}';
+             ").fetch_row[0]
+  end
 end
